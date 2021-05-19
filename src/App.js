36d31ed66d501032e0280d394e6f689a81b11dbd
sshimabuku <sashi.shimabuku@gmail.com>
2021-05-18 21:35:58 -0400

    SR-0001 -- Display current sr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import CurrentSkillRanking from './components/CurrentSkillRanking'
+
 function App() {
+  const skillRankings = [1500, 2500, 3500, 4500];
+
+  const getCurrentSkillRanking = () => {
+    const position = skillRankings.length - 1;
+    return skillRankings[position];
+  }
+
   return (
     <div className="App">
+      <CurrentSkillRanking
+        skillRanking={getCurrentSkillRanking()} />
     </div>
   );
 }
---
+++ src/components/CurrentSkillRanking.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CurrentSkillRanking = ({ skillRanking }) => {
+  return (
+    <section id="current-sr">
+      <h2>Current SR</h2>
+      <p>{skillRanking}</p>
+    </section>
+  )
+}
+
+export default CurrentSkillRanking;
